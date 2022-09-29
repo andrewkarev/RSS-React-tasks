d133476b8577295605fc4418597e854a92d135f6
Andrew <an.carev4@gmail.com>
2022-09-29 17:16:06 +0300

    feat: stylize `Footer`

--- project/src/components/footer/Footer.tsx
+++ project/src/components/footer/Footer.tsx
@@ -3,7 +3,14 @@ import styles from './footer.module.css';
 
 class Footer extends React.Component {
   render() {
-    return <footer className={styles.footer}>2022</footer>;
+    return (
+      <div className="container">
+        <footer className={styles.footer}>
+          <p className={styles['footer-item']}>2022</p>
+          <p className={styles['footer-item']}>“Sometimes science is more art than science.”</p>
+        </footer>
+      </div>
+    );
   }
 }
 
