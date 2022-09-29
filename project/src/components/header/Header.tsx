e43ffc8c1874235607e0e26bc4e5f949b37817e5
Andrew <an.carev4@gmail.com>
2022-09-29 17:30:49 +0300

    feat: stylize `Header` component

--- project/src/components/header/Header.tsx
+++ project/src/components/header/Header.tsx
@@ -1,3 +1,4 @@
+import SearchField from 'components/search-field/SearchField';
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './header.module.css';
@@ -6,8 +7,29 @@ class Header extends React.Component {
   render() {
     return (
       <header className={styles.header}>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="about">About</NavLink>
+        <div className="container">
+          <div className={styles['header-content-wrapper']}>
+            <nav className={styles['header-links']}>
+              <div className={styles['link-container']}>
+                <NavLink
+                  to="main"
+                  className={({ isActive }) => (isActive ? styles['link-active'] : styles.link)}
+                >
+                  Home
+                </NavLink>
+              </div>
+              <div className={styles['link-container']}>
+                <NavLink
+                  to="about"
+                  className={({ isActive }) => (isActive ? styles['link-active'] : styles.link)}
+                >
+                  About
+                </NavLink>
+              </div>
+            </nav>
+            <SearchField />
+          </div>
+        </div>
       </header>
     );
   }
