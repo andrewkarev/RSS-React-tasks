d85271221569a33ea2b8812cb545fcd47cd87d4c
Andrew <an.carev4@gmail.com>
2022-09-29 17:01:59 +0300

    feat: stylize `Not found page`

---
+++ project/src/pages/not-found-page/NotFoundPage.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styles from './not-found-page.module.css';
+import NOT_FOUND_PAGE_IMG from './../../assets/images/not-found.png';
+
+class NotFoundPage extends React.Component {
+  render() {
+    return (
+      <div className={styles['not-found-page']}>
+        <h2 className={styles['not-found-page-title']}>Page not found</h2>
+        <p className={styles['not-found-page-message']}>
+          We couldn&apos;t find what you were looking for.
+        </p>
+        <p className={styles['not-found-page-message']}>
+          Please contact the owner of the site that linked you to the original URL and let them know
+          their link is broken.
+        </p>
+        <img
+          className={styles['not-found-page-image']}
+          src={NOT_FOUND_PAGE_IMG}
+          alt="Rick and Morty"
+        />
+      </div>
+    );
+  }
+}
+
+export default NotFoundPage;
