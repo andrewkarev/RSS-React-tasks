c6769ed1a4bcd187a83c9dc0f243f83f62fba989
Andrew <an.carev4@gmail.com>
2022-10-02 22:03:18 +0300

    refactor: relocate `BrowserRouter` to the index

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import AboutPage from './pages/about-page/AboutPage';
 import MainPage from 'pages/main-page/MainPage';
@@ -9,16 +9,14 @@ import Layout from 'components/Layout';
 class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<MainPage />} />
-            <Route path="about" element={<AboutPage />} />
-            <Route path="404" element={<NotFoundPage />} />
-            <Route path="*" element={<Navigate to="404" replace />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainPage />} />
+          <Route path="about" element={<AboutPage />} />
+          <Route path="404" element={<NotFoundPage />} />
+          <Route path="*" element={<Navigate to="404" replace />} />
+        </Route>
+      </Routes>
     );
   }
 }
