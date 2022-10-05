4b5d0da87029611dff87d7c0ce3485f7aa921c05
Andrew <an.carev4@gmail.com>
2022-10-05 23:31:00 +0300

    feat: stylize `FormPage`

--- src/pages/form-page/FormPage.tsx
+++ src/pages/form-page/FormPage.tsx
@@ -1,9 +1,18 @@
+import Form from 'components/form/Form';
 import React from 'react';
 import styles from './form-page.module.css';
 
 class FormPage extends React.Component {
   render() {
-    return <div className={styles['form-page']}></div>;
+    return (
+      <div className={styles['form-page']} data-testid={'form'}>
+        <h2 className={styles['form-page-title']}>Create new character</h2>
+        <div className={styles['form-container']}>
+          <Form />
+        </div>
+        <div className={styles['cards-container']}></div>
+      </div>
+    );
   }
 }
 
