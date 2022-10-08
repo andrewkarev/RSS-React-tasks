70058e8281c990bc0e88b3279d78ec77fb7a7039
Andrew <an.carev4@gmail.com>
2022-10-08 18:04:29 +0300

    feat: implement cards displaying logic

--- src/pages/form-page/FormPage.tsx
+++ src/pages/form-page/FormPage.tsx
@@ -1,16 +1,34 @@
-import Form from 'components/form/Form';
+import Form from 'components/form';
 import React from 'react';
 import styles from './form-page.module.css';
 
-class FormPage extends React.Component {
+interface FormPageState {
+  cards: JSX.Element[];
+}
+
+class FormPage extends React.Component<Record<string, never>, FormPageState> {
+  constructor(props: Record<string, never>) {
+    super(props);
+    this.state = {
+      cards: [],
+    };
+    this.addNewCards = this.addNewCards.bind(this);
+  }
+
+  addNewCards(newCard: JSX.Element) {
+    this.setState(({ cards }) => {
+      return { cards: [...cards, newCard] };
+    });
+  }
+
   render() {
     return (
       <div className={styles['form-page']} data-testid={'form'}>
         <h2 className={styles['form-page-title']}>Create new character</h2>
         <div className={styles['form-container']}>
-          <Form />
+          <Form addNewCards={this.addNewCards} />
         </div>
-        <div className={styles['cards-container']}></div>
+        <div className={styles['cards-container']}>{this.state.cards}</div>
       </div>
     );
   }
