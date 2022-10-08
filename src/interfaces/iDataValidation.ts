1ee39f8838d317ae13109b75731906e43c14ec61
Andrew <an.carev4@gmail.com>
2022-10-08 18:06:10 +0300

    feat: implement interface for form fields validator function

---
+++ src/interfaces/iDataValidation.ts
@@ -0,0 +1,12 @@
+interface IDataValidation {
+  currentNameValue: string;
+  currentStatusValue: string;
+  currentSpeciesValue: string;
+  currentGenderValue: string;
+  currentOriginValue: string;
+  currentLocationValue: string;
+  currentDateOfCreationValue: string;
+  currentAvatarValue: FileList;
+}
+
+export default IDataValidation;
