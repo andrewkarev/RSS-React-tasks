3a3e72de3db01fb8c7180885b360858931130bce
Andrew <an.carev4@gmail.com>
2022-10-08 18:07:49 +0300

    fix: update `CardProps` interface

--- src/interfaces/CardProps.ts
+++ src/interfaces/CardProps.ts
@@ -1,21 +1,21 @@
 interface CardProps {
-  id: number;
+  id?: number;
   name: string;
   status: string;
   species: string;
-  type: string;
+  type?: string;
   gender: string;
   origin: {
     name: string;
-    url: string;
+    url?: string;
   };
   location: {
     name: string;
-    url: string;
+    url?: string;
   };
   image: string;
-  episode: string[];
-  url: string;
+  episode?: string[];
+  url?: string;
   created: string;
 }
 
