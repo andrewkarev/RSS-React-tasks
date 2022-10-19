6dae5e067dcbbcafc2fdf106c314ea68a51fbfbf
Andrew <an.carev4@gmail.com>
2022-10-19 13:21:00 +0300

    init: start Hooks task

--- src/pages/about-page/AboutPage.tsx
+++ src/pages/about-page/AboutPage.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import styles from './about-page.module.css';
 
-class AboutPage extends React.Component {
-  render() {
-    return (
-      <div className={styles['about']} data-testid={'about'}>
-        <p>
-          Hello, my name is Andrew and I&apos;m interested in creating fun and well designed apps.
-        </p>
-      </div>
-    );
-  }
-}
+const AboutPage: React.FC = () => {
+  return (
+    <div className={styles['about']} data-testid={'about'}>
+      <p>
+        Hello, my name is Andrew and I&apos;m interested in creating fun and well designed apps.
+      </p>
+    </div>
+  );
+};
 
 export default AboutPage;
