2c4b1c71ff256ccc7952500af2dee2cf921e8b85
Andrew <an.carev4@gmail.com>
2022-10-20 17:40:30 +0300

    fix: move card creation logic on form submit to the `FormPage`

--- src/pages/form-page/FormPage.tsx
+++ src/pages/form-page/FormPage.tsx
@@ -1,3 +1,4 @@
+import Card from 'components/card';
 import Form from 'components/form';
 import PopUp from 'components/pop-up/PopUp';
 import ICard from 'interfaces/ICard';
@@ -12,25 +13,31 @@ interface FormPageProps {
 }
 
 const FormPage: React.FC<FormPageProps> = (props) => {
-  const [cards, setCards] = useState<JSX.Element[]>([]);
+  const [cards, setCards] = useState<ICard[]>([]);
 
-  const addNewCards = (newCard: JSX.Element) => {
+  const addNewCards = (newCard: ICard) => {
     setCards((prevCards) => [...prevCards, newCard]);
   };
 
   const popUp = <PopUp card={props.selectedCard} toggleModalWindow={props.toggleModalWindow} />;
+  const cardsElement = cards.map((card, i) => {
+    return (
+      <Card
+        card={card}
+        toggleModalWindow={props.toggleModalWindow}
+        setSelectedCardValue={props.setSelectedCardValue}
+        key={i}
+      />
+    );
+  });
 
   return (
     <div className={styles['form-page']} data-testid={'form-page'}>
       <h2 className={styles['form-page-title']}>Create new character</h2>
       <div className={styles['form-container']}>
-        <Form
-          addNewCards={addNewCards}
-          setSelectedCardValue={props.setSelectedCardValue}
-          toggleModalWindow={props.toggleModalWindow}
-        />
+        <Form addNewCards={addNewCards} />
       </div>
-      <div className={styles['cards-container']}>{cards}</div>
+      <div className={styles['cards-container']}>{cardsElement}</div>
       {props.isModalOpened && popUp}
     </div>
   );
