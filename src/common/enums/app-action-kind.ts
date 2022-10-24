236e538f46f66ad7366cfff9f5de0814deed861c
Andrew <an.carev4@gmail.com>
2022-10-24 17:48:37 +0300

    feat: implement enum with reducer actions types

---
+++ src/common/enums/app-action-kind.ts
@@ -0,0 +1,8 @@
+enum AppActionKind {
+  'SET_FORM_PAGE_CARDS' = 'setFormPageCards',
+  'SET_SEARCH_FIELD_VALUE' = 'setSearchFieldValue',
+  'SET_SEARCH_QUERY' = 'setSearchQuery',
+  'SET_FORM_FIELDS_VALUES' = 'setFormFieldsValues',
+}
+
+export default AppActionKind;
