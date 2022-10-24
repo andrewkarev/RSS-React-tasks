9226ae1212c9952d88b5ab536b9cc9b9e39f78f7
Andrew <an.carev4@gmail.com>
2022-10-24 17:50:04 +0300

    feat: implement app context

---
+++ src/context/AppContext.tsx
@@ -0,0 +1,40 @@
+import AppAction from 'interfaces/AppAction';
+import AppState from 'interfaces/AppState';
+import React from 'react';
+import { appReducer, INITIAL_STATE } from './app-reducer';
+
+type Dispatch = ({ type, payload }: AppAction) => void;
+type AppProviderProps = { children: React.ReactNode };
+
+const AppStateContext = React.createContext<AppState | undefined>(undefined);
+const AppDispatchContext = React.createContext<Dispatch | undefined>(undefined);
+
+const AppProvider = ({ children }: AppProviderProps) => {
+  const [state, dispatch] = React.useReducer(appReducer, INITIAL_STATE);
+
+  return (
+    <AppStateContext.Provider value={state}>
+      <AppDispatchContext.Provider value={dispatch}>{children}</AppDispatchContext.Provider>
+    </AppStateContext.Provider>
+  );
+};
+
+const useAppState = () => {
+  const context = React.useContext(AppStateContext);
+
+  if (context === undefined) {
+    throw new Error('useAppState must be used within a AppProvider');
+  }
+  return context;
+};
+
+const useAppDispatch = () => {
+  const context = React.useContext(AppDispatchContext);
+
+  if (context === undefined) {
+    throw new Error('useAppDispatch must be used within a AppProvider');
+  }
+  return context;
+};
+
+export { AppProvider, useAppState, useAppDispatch };
