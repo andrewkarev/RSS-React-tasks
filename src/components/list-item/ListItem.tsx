3029b63afc6c82bfd3ea4fc3e8b0600be5526fdd
Andrew <an.carev4@gmail.com>
2022-10-27 18:23:36 +0300

    fix: update `ListItem` stylisation

--- src/components/list-item/ListItem.tsx
+++ src/components/list-item/ListItem.tsx
@@ -1,4 +1,6 @@
+import AppPathesEnum from 'common/enums/app-pathes';
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import styles from './list-item.module.css';
 
 interface ListItemProps {
@@ -7,10 +9,42 @@ interface ListItemProps {
 }
 
 const ListItem: React.FC<ListItemProps> = ({ annotation, info }) => {
+  const location = useLocation();
+
   return (
-    <li className={styles['list-item']}>
-      <p className={styles['list-item-annotation']}>{annotation}</p>
-      <p className={styles['list-item-info']}>{info}</p>
+    <li
+      className={
+        styles[
+          `${location.pathname.includes(AppPathesEnum.form) ? 'list-item-modal' : 'list-item'}`
+        ]
+      }
+    >
+      <p
+        className={
+          styles[
+            `${
+              location.pathname.includes(AppPathesEnum.form)
+                ? 'list-item-annotation-modal'
+                : 'list-item-annotation'
+            }`
+          ]
+        }
+      >
+        {annotation}
+      </p>
+      <p
+        className={
+          styles[
+            `${
+              location.pathname.includes(AppPathesEnum.form)
+                ? 'list-item-info-modal'
+                : 'list-item-info'
+            }`
+          ]
+        }
+      >
+        {info}
+      </p>
     </li>
   );
 };
