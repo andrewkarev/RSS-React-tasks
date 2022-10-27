cc69d4788d4eeca8945b84b27ae40cb6bc56c8bd
Andrew <an.carev4@gmail.com>
2022-10-27 18:31:31 +0300

    feat: implement `CardInfoPage` component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import AboutPage from './pages/about-page';
@@ -6,58 +6,18 @@ import MainPage from 'pages/main-page';
 import NotFoundPage from 'pages/not-found-page';
 import Layout from 'components';
 import FormPage from 'pages/form-page';
-import ICard from 'interfaces/ICard';
 import AppPathesEnum from 'common/enums/app-pathes';
+import CardInfoPage from 'pages/card-info-page';
 
 const App: React.FC = () => {
-  const [selectedCard, setSelectedCard] = useState<ICard | null>(null);
-  const [isModalOpened, setIsModalOpened] = useState<boolean>(false);
-
-  const setSelectedCardValue = (card: ICard) => {
-    setSelectedCard(() => card);
-  };
-
-  const toggleModalWindow = () => {
-    const body = document.querySelector('body');
-
-    if (!body) return;
-
-    if (!isModalOpened) {
-      body.style.overflow = 'hidden';
-    } else {
-      body.style.overflow = 'auto';
-    }
-
-    setIsModalOpened((prevModalState) => !prevModalState);
-  };
-
   return (
     <Routes>
       <Route path={AppPathesEnum.home} element={<Layout />}>
-        <Route
-          index
-          element={
-            <MainPage
-              selectedCard={selectedCard}
-              isModalOpened={isModalOpened}
-              setSelectedCardValue={setSelectedCardValue}
-              toggleModalWindow={toggleModalWindow}
-            />
-          }
-        />
+        <Route index element={<MainPage />} />
         <Route path={AppPathesEnum.about} element={<AboutPage />} />
-        <Route
-          path={AppPathesEnum.form}
-          element={
-            <FormPage
-              selectedCard={selectedCard}
-              isModalOpened={isModalOpened}
-              setSelectedCardValue={setSelectedCardValue}
-              toggleModalWindow={toggleModalWindow}
-            />
-          }
-        />
+        <Route path={AppPathesEnum.form} element={<FormPage />} />
         <Route path={AppPathesEnum.notFound} element={<NotFoundPage />} />
+        <Route path={`${AppPathesEnum.character}/:id`} element={<CardInfoPage />} />
         <Route
           path={AppPathesEnum.redirect}
           element={<Navigate to={AppPathesEnum.notFound} replace />}
---
+++ src/pages/card-info-page/CardInfoPage.tsx
@@ -0,0 +1,103 @@
+import AppPathesEnum from 'common/enums/app-pathes';
+import ListItem from 'components/list-item';
+import Loader from 'components/loader/';
+import { useAppState } from 'context/AppContext';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import getSeries from 'services/get-series-api';
+import styles from './card-info-page.module.css';
+
+export const CardInfoPage: React.FC = () => {
+  const appState = useAppState();
+
+  const [firstEpisodeName, setFirstEpisodeName] = useState('');
+  const [lastEpisodeName, setLastEpisodeName] = useState('');
+
+  const [isPending, setIsPending] = useState<boolean>(true);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const updateCards = async () => {
+      const firstEpisodeLink = appState.selectedCard?.episode?.at(0);
+      const lastEpisodeLink = appState.selectedCard?.episode?.at(-1);
+
+      if (!firstEpisodeLink || !lastEpisodeLink) {
+        navigate(AppPathesEnum.home);
+        return;
+      }
+
+      try {
+        setIsPending(() => true);
+
+        const firstEpisode = await getSeries(firstEpisodeLink);
+        const lastEpisode = await getSeries(lastEpisodeLink);
+
+        setFirstEpisodeName(() => firstEpisode.name || '');
+        setLastEpisodeName(() => lastEpisode.name || '');
+
+        setIsPending(() => false);
+      } catch (error) {
+        console.log('error');
+        setIsPending(() => false);
+      }
+    };
+
+    updateCards();
+  }, [navigate, appState.selectedCard?.episode]);
+
+  const characterInfo = (
+    <div className={styles['card-info-page']}>
+      <div className={styles['card-info-page-left-side']}>
+        <div className={styles['img-wrapper']}>
+          <img
+            className={styles['img']}
+            src={appState.selectedCard?.image}
+            alt="Character avatar"
+          />
+        </div>
+        <h3 className={styles['name']}>{appState.selectedCard?.name}</h3>
+      </div>
+      <div className={styles['card-info-page-right-side']}>
+        <ul className={styles['list']}>
+          <li className={styles['list-item']}>
+            <div
+              className={
+                styles[
+                  `${
+                    appState.selectedCard?.status === 'Alive'
+                      ? 'marker-alive'
+                      : appState.selectedCard?.status === 'Dead'
+                      ? 'marker-dead'
+                      : 'marker'
+                  }`
+                ]
+              }
+            ></div>
+            <p className={styles['list-item-info']}>
+              {appState.selectedCard?.status} - {appState.selectedCard?.species}
+            </p>
+          </li>
+          <ListItem annotation={'Place of origin:'} info={appState.selectedCard?.origin?.name} />
+          <ListItem annotation={'First seen in:'} info={firstEpisodeName} />
+          <ListItem annotation={'Last seen in:'} info={lastEpisodeName} />
+          <ListItem
+            annotation={'Last known location:'}
+            info={appState.selectedCard?.location?.name}
+          />
+        </ul>
+        <button
+          className={styles['back-btn']}
+          type="button"
+          onClick={() => navigate(AppPathesEnum.home)}
+        >
+          Go back
+        </button>
+      </div>
+    </div>
+  );
+
+  return <>{isPending ? <Loader /> : characterInfo}</>;
+};
+
+export default CardInfoPage;
