f9b91a552494bbc8f3181231c510ca25b8d8657b
Andrew <an.carev4@gmail.com>
2022-10-28 20:45:33 +0300

    feat: add button for sending request with search field data

--- src/components/search-field/SearchField.tsx
+++ src/components/search-field/SearchField.tsx
@@ -16,17 +16,29 @@ const SearchField: React.FC = () => {
     });
   };
 
+  const handleClick = () => {
+    appDispatch({
+      type: AppActionKind.SET_SEARCH_QUERY,
+      payload: { searchQuery: appState.searchFieldValue },
+    });
+  };
+
   return (
-    <input
-      className={styles['input']}
-      type="text"
-      name="search"
-      placeholder="Search"
-      autoComplete="off"
-      autoFocus={true}
-      value={appState.searchFieldValue}
-      onChange={handleChange}
-    />
+    <>
+      <input
+        className={styles['input']}
+        type="text"
+        name="search"
+        placeholder="Search"
+        autoComplete="off"
+        autoFocus={true}
+        value={appState.searchFieldValue}
+        onChange={handleChange}
+      />
+      <button className={styles['submit-btn']} type="button" onClick={handleClick}>
+        Find
+      </button>
+    </>
   );
 };
 
