31bcc9b69e7462c8d8eca892e31f1a117a3aaf56
Andrew <an.carev4@gmail.com>
2022-10-29 17:17:57 +0300

    refactor: update information to display in the header on the page with card details

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,46 +1,41 @@
-import AppPathesEnum from 'common/enums/app-pathes';
 import React from 'react';
-import { NavLink, useLocation, useParams } from 'react-router-dom';
+import AppPathesEnum from 'common/enums/app-pathes';
+import CustomLink from 'components/custom-link';
+import { useAppState } from 'context/AppContext';
+import { useLocation } from 'react-router-dom';
 import styles from './header.module.css';
 
 const Header: React.FC = () => {
   const location = useLocation();
-  const { id } = useParams();
+  const appState = useAppState();
+
+  const position =
+    appState.mainPageCards.findIndex((card) => card.id === appState.selectedCard?.id) + 1;
+
+  const positionElement = (
+    <div className={styles['position-info']}>
+      <p>
+        <span className={styles['info-title']}>Page: </span>
+        {appState.mainPageControlsValues.pageNumber}
+      </p>
+      <div className={styles['vertical-line']}></div>
+      <p>
+        <span className={styles['info-title']}>Position: </span>
+        {position}
+      </p>
+    </div>
+  );
 
   return (
     <header className={styles['header']} data-testid={'header'}>
       <div className="container">
         <div className={styles['header-content-wrapper']}>
           <nav className={styles['header-links']}>
-            <div className={styles['link-container']}>
-              <NavLink
-                to={AppPathesEnum.home}
-                className={({ isActive }) => (isActive ? styles['link-active'] : styles['link'])}
-                end
-              >
-                Home
-              </NavLink>
-            </div>
-            <div className={styles['link-container']}>
-              <NavLink
-                to={AppPathesEnum.about}
-                className={({ isActive }) => (isActive ? styles['link-active'] : styles['link'])}
-              >
-                About
-              </NavLink>
-            </div>
-            <div className={styles['link-container']}>
-              <NavLink
-                to={AppPathesEnum.form}
-                className={({ isActive }) => (isActive ? styles['link-active'] : styles['link'])}
-              >
-                Create
-              </NavLink>
-            </div>
+            <CustomLink path={AppPathesEnum.home} title={'Home'} />
+            <CustomLink path={AppPathesEnum.about} title={'About'} />
+            <CustomLink path={AppPathesEnum.form} title={'Create'} />
           </nav>
-          {location.pathname.includes(AppPathesEnum.character) && (
-            <div className={styles['position']}>{id}</div>
-          )}
+          {location.pathname.includes(AppPathesEnum.character) && positionElement}
         </div>
       </div>
     </header>
