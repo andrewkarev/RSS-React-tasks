ae36c06f5b0aadd8b1ff83e7e1e7ec6461fbe029
Andrew <an.carev4@gmail.com>
2022-10-29 17:16:25 +0300

    feat: implement `CustomLink` component

---
+++ src/components/custom-link/CustomLink.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import AppPathesEnum from 'common/enums/app-pathes';
+import styles from './custom-link.module.css';
+
+interface CustomLinkProps {
+  path: AppPathesEnum;
+  title: string;
+}
+
+const CustomLink: React.FC<CustomLinkProps> = ({ path, title }) => {
+  return (
+    <div className={styles['link-container']}>
+      <NavLink
+        to={path}
+        className={({ isActive }) => (isActive ? styles['link-active'] : styles['link'])}
+        end
+      >
+        {title}
+      </NavLink>
+    </div>
+  );
+};
+
+export default CustomLink;
