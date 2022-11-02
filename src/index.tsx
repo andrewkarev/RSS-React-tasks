e5c6f88d6e468f1b8ba74c63868f7fbd1aeb2e3e
Andrew <an.carev4@gmail.com>
2022-11-02 15:41:54 +0300

    feat: set up store

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,19 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { AppProvider } from 'context/AppContext';
+import { Provider } from 'react-redux';
+import { store } from 'store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AppProvider>
-        <App />
-      </AppProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <AppProvider>
+          <App />
+        </AppProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
